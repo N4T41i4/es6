112f4fd8f7ae7221c834c6118aae5a377256d366
Natalia <natalia@example.com>
2024-01-21 10:26:28 -0300

    es6

---
+++ dist/dever.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var nome = ['Natalia', 'Jorge', 'Lucas'];
+var alunos = nome.map(function (item) {
+  return {
+    nome: item,
+    nota: 7
+  };
+});
+alunos.push({
+  nome: 'Sergio',
+  nota: 6
+});
+alunos.push({
+  nome: 'Lúcio',
+  nota: 4
+});
+function notaMaior(item) {
+  return item.nota === 6 || item.nota > 6;
+}
+;
+var aluno = alunos.filter(notaMaior);
+console.log(aluno);
